822c6390e26ae9f749f3699663c24cdc64f1efc9
Ngoc Thieu <54256540+ngocthieuff@users.noreply.github.com>
2023-12-07 09:14:55 +0700

    Translate module E* part 1 (#5)
    * translate EAPM
    
    * translate EmailAddresses
    
    * translate EmailMan
    
    * translate EmailMarketing
    
    * Update and Fixed kh_CA.lang.php
    
    ---------
    
    Co-authored-by: ChannboreyMoul00 <152970372+ChannboreyMoul00@users.noreply.github.com>

--- modules/EmailAddresses/language/kh_CA.lang.php
+++ modules/EmailAddresses/language/kh_CA.lang.php
@@ -1,17 +1,17 @@
 <?php
 
 $mod_strings=array (
-  'LBL_CONFIRMATION_MESSAGE' => 'Your subscription has been confirmed. You have been added to our list and will hear from us soon.',
-  'LBL_CONFIRMATION_REQUESTED_ON' => 'Confirmation Request Action Taken',
-  'LBL_CONFIRMATION_TITLE' => 'Thank You',
-  'LBL_DATE_CREATE' => 'Date Create',
-  'LBL_DATE_MODIFIED' => 'Date Modified',
-  'LBL_DELETED' => 'Delete',
-  'LBL_EMAIL_ADDRESS' => 'Email Address',
-  'LBL_EMAIL_ADDRESS_CAPS' => 'Email Address caps',
+  'LBL_CONFIRMATION_MESSAGE' => 'ការជាវរបស់អ្នកត្រូវបានបញ្ជាក់។ អ្នកត្រូវបានបញ្ចូលទៅក្នុងបញ្ជីរបស់យើង ហើយនឹងឮពីពួកយើងក្នុងពេលឆាប់ៗនេះ។'
+  'LBL_CONFIRMATION_REQUESTED_ON' => 'ការបញ្ជាក់សំណើរធ្វើសកម្មភាព',
+  'LBL_CONFIRMATION_TITLE' => 'សូមអរគុណ',
+  'LBL_DATE_CREATE' => 'កាលបរិច្ឆេទបង្កើត',
+  'LBL_DATE_MODIFIED' => 'កាលបរិច្ឆេទដែលបានកែប្រែ',
+  'LBL_DELETED' => 'លុប',
+  'LBL_EMAIL_ADDRESS' => 'អាសយដ្ឋានអ៊ីមែល',
+  'LBL_EMAIL_ADDRESS_CAPS' => 'មួកអាសយដ្ឋានអ៊ីមែល',
   'LBL_EMAIL_ADDRESS_ID' => 'ID',
-  'LBL_INVALID_EMAIL' => 'Invalid Email',
-  'LBL_MODULE_NAME' => 'Email Addresses',
-  'LBL_MODULE_NAME_SINGULAR' => 'Email Address',
-  'LBL_OPT_OUT' => 'Opted Out',
+  'LBL_INVALID_EMAIL' => 'អ៊ីមែលមិនត្រឹមត្រូវ',
+  'LBL_MODULE_NAME' => 'អាសយដ្ឋានអ៊ីមែល',
+  'LBL_MODULE_NAME_SINGULAR' => 'អាសយដ្ឋានអ៊ីមែល',
+  'LBL_OPT_OUT' => 'បានជ្រើសរើសចេញ',
 );
