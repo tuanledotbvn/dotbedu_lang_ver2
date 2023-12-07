a398540e6556bb5970c9b10935ad08deb9df33f8
ngocthieuff <ptnngoc22@gmail.com>
2023-12-07 10:15:03 +0700

    Expression

--- modules/ExpressionEngine/language/kh_CA.lang.php
+++ modules/ExpressionEngine/language/kh_CA.lang.php
@@ -1,11 +1,11 @@
 <?php
 
 $mod_strings=array (
-  'LBL_EXPAND' => 'Expand',
-  'LBL_MODULE_NAME' => 'Expression Engine',
-  'LBL_MODULE_NAME_SINGULAR' => 'Expression Engine',
-  'LBL_NEXT' => 'Next',
-  'LBL_PREVIOUS' => 'Previous',
+  'LBL_EXPAND' => 'ធេវីអាេយយារ',
+  'LBL_MODULE_NAME' => 'ម៉ាស៊ីនបញ្ចេញមតិ',
+  'LBL_MODULE_NAME_SINGULAR' => 'ម៉ាស៊ីនបញ្ចេញមតិ',
+  'LBL_NEXT' => 'បន្ទាប់បិនេហ',
+  'LBL_PREVIOUS' => 'ចាដមបុង',
   'func_descriptions' => 
   array (
   ),
