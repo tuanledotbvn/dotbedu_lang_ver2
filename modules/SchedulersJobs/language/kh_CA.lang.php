2898bb57024d816328a0bbf6c2b463741e1696cc
ngocthieuff <ptnngoc22@gmail.com>
2023-12-07 14:41:51 +0700

    update

--- modules/SchedulersJobs/language/kh_CA.lang.php
+++ modules/SchedulersJobs/language/kh_CA.lang.php
@@ -1,74 +1,74 @@
 <?php
 
 $mod_strings=array (
-  'ERR_CALL' => 'Cannot call function: %s',
-  'ERR_CONFIG_EMPTY_FIELDS' => 'Some of the fields are empty',
-  'ERR_CONFIG_MISSING_EXTENSION' => 'Extension for this queue is not installed',
-  'ERR_CURL' => 'No CURL - cannot run URL jobs',
-  'ERR_FAILED' => 'Unexpected failure, please check PHP logs and dotbcrm.log',
-  'ERR_JOBTYPE' => 'Unknown job type: %s',
-  'ERR_JOB_FAILED_VERBOSE' => 'Job %1$s (%2$s) failed in CRON run',
-  'ERR_NOSUCHUSER' => 'User ID %s not found',
-  'ERR_NOUSER' => 'No User ID specified for the job',
-  'ERR_PHP' => '%s [%d]: %s in %s on line %d',
-  'ERR_TIMEOUT' => 'Forced failure on timeout',
-  'ERR_WORKER_CANNOT_LOAD_BEAN' => 'Cannot load bean with id: %s',
-  'ERR_WORKER_NO_REGISTERED_FUNCTIONS' => 'Cannot find handler for route %s',
-  'LBL_ACCESS_KEY' => 'Access Key ID',
-  'LBL_ACCESS_KEY_SECRET' => 'Secret Access Key',
-  'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
-  'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
-  'LBL_CLIENT' => 'Owning client',
-  'LBL_CONFIG_AMAZON_SQS_SECTION' => 'Amazon-sqs Configuration',
-  'LBL_CONFIG_AMAZON_SQS_TEXT' => '<p><b>Amazon SQS Configuration Section.</b></p>
+  'ERR_CALL' => 'មិនអាចហៅមុខងារ៖ %s',
+  'ERR_CONFIG_EMPTY_FIELDS' => 'វាលខ្លះទទេ',
+  'ERR_CONFIG_MISSING_EXTENSION' => 'ផ្នែកបន្ថែមសម្រាប់ជួរនេះមិនត្រូវបានដំឡើងទេ',
+  'ERR_CURL' => 'គ្មាន CURL - មិនអាចដំណើរការការងារ URL',
+  'ERR_FAILED' => 'ការបរាជ័យដែលមិនបានរំពឹងទុក សូមពិនិត្យមើលកំណត់ហេតុ PHP និង dotbcrm.log',
+  'ERR_JOBTYPE' => 'មិនស្គាល់ប្រភេទការងារ៖ %s',
+  'ERR_JOB_FAILED_VERBOSE' => 'ការងារ %1$s (%2$s) បានបរាជ័យក្នុងការដំណើរការ CRON',
+  'ERR_NOSUCHUSER' => 'រកមិនឃើញលេខសម្គាល់អ្នកប្រើប្រាស់ %s',
+  'ERR_NOUSER' => 'គ្មាន​លេខ​សម្គាល់​អ្នក​ប្រើ​ដែល​បញ្ជាក់​សម្រាប់​ការងារ',
+  'ERR_PHP' => '%s [%d]: %s ក្នុង %s នៅលើបន្ទាត់ %d',
+  'ERR_TIMEOUT' => 'បង្ខំឱ្យបរាជ័យពេលអស់ពេល',
+  'ERR_WORKER_CANNOT_LOAD_BEAN' => 'មិនអាចផ្ទុកសណ្តែកដែលមានលេខសម្គាល់៖ %s',
+  'ERR_WORKER_NO_REGISTERED_FUNCTIONS' => 'មិនអាចស្វែងរកអ្នកដោះស្រាយសម្រាប់ផ្លូវ %s',
+  'LBL_ACCESS_KEY' => 'លេខសម្គាល់សោចូល',
+  'LBL_ACCESS_KEY_SECRET' => 'សោចូលសម្ងាត់',
+  'LBL_ASSIGNED_TO_ID' => 'លេខសម្គាល់អ្នកប្រើប្រាស់ដែលបានចាត់តាំង',
+  'LBL_ASSIGNED_TO_NAME' => 'បានចាត់តាំងឱ្យ',
+  'LBL_CLIENT' => 'អតិថិជនជាម្ចាស់',
+  'LBL_CONFIG_AMAZON_SQS_SECTION' => 'ការកំណត់រចនាសម្ព័ន្ធ Amazon-sqs',
+  'LBL_CONFIG_AMAZON_SQS_TEXT' => '<p><b>ផ្នែកកំណត់រចនាសម្ព័ន្ធ Amazon SQS ។</b></p>
 <ul>
-    <li>Access Key ID: <i>Enter your access key id number for Amazon SQS</i></li>
-    <li>Secret Access Key: <i>Enter your secret access key for Amazon SQS</i></li>
-    <li>Region: <i>Enter the region of Amazon SQS server</i></li>
-    <li>Queue Name: <i>Enter queue name of Amazon SQS server</i></li>
+    <li>ចូលប្រើកូនសោ ID: <i>បញ្ចូលលេខសម្គាល់សោចូលដំណើរការរបស់អ្នកសម្រាប់ Amazon SQS</i></li>
+    <li>Secret Access Key៖ <i>បញ្ចូលសោចូលសម្ងាត់របស់អ្នកសម្រាប់ Amazon SQS</i></li>
+    <li>តំបន់៖ <i>បញ្ចូលតំបន់នៃម៉ាស៊ីនមេ Amazon SQS</i></li>
+    <li>ឈ្មោះជួរ៖ <i>បញ្ចូលឈ្មោះជួរនៃម៉ាស៊ីនមេ Amazon SQS</i></li>
 </ul>',
-  'LBL_CONFIG_AMAZON_SQS_TITLE' => 'Amazon SQS Configuration Help',
-  'LBL_CONFIG_AMQP_SECTION' => 'AMQP Configuration',
-  'LBL_CONFIG_AMQP_TEXT' => '<p><b>AMQP Configuration Section.</b></p>
+'LBL_CONFIG_AMAZON_SQS_TITLE' => 'ជំនួយការកំណត់រចនាសម្ព័ន្ធ Amazon SQS',
+  'LBL_CONFIG_AMQP_SECTION' => 'ការកំណត់រចនាសម្ព័ន្ធ AMQP',
+  'LBL_CONFIG_AMQP_TEXT' => '<p><b>ផ្នែកកំណត់រចនាសម្ព័ន្ធ AMQP។</b></p>
 <ul>
-    <li>Server URL: <i>Enter your message queue server\'s URL.</i></li>
-    <li>Login: <i>Enter your login for RabbitMQ</i></li>
-    <li>Password: <i>Enter your password for RabbitMQ</i></li>
+     <li>URL របស់ម៉ាស៊ីនមេ៖ <i>បញ្ចូល URL របស់ម៉ាស៊ីនមេជួរសាររបស់អ្នក។</i></li>
+     <li>ចូល៖ <i>បញ្ចូលការចូលរបស់អ្នកសម្រាប់ RabbitMQ</i></li>
+     <li>ពាក្យសម្ងាត់៖ <i>បញ្ចូលពាក្យសម្ងាត់របស់អ្នកសម្រាប់ RabbitMQ</i></li>
 </ul>',
   'LBL_CONFIG_AMQP_TITLE' => 'AMQP Configuration Help',
   'LBL_CONFIG_GEARMAN_SECTION' => 'Gearman Configuration',
   'LBL_CONFIG_GEARMAN_TEXT' => '<p><b>Gearman Configuration Section.</b></p>
 <ul>
-    <li>Server URL: <i>Enter your message queue server\'s URL.</i></li>
+     <li>Server URL: <i>បញ្ចូល URL របស់ម៉ាស៊ីនមេជួរសាររបស់អ្នក។</i></li>
 </ul>',
   'LBL_CONFIG_GEARMAN_TITLE' => 'Gearman Configuration Help',
   'LBL_CONFIG_MAIN_SECTION' => 'Main Configuration',
   'LBL_CONFIG_PAGE' => 'Job Queue Settings',
   'LBL_CONFIG_QUEUE_MANAGER' => 'Runner',
   'LBL_CONFIG_QUEUE_TYPE' => 'Adapter',
-  'LBL_CONFIG_SERVERS_TEXT' => '<p><b>Main Configuration Section.</b></p>
-<ul>
-    <li>Runner:
-    <ul>
-    <li><i>Standard</i> - use only one process for workers.</li>
-    <li><i>Parallel</i> - use a few processes for workers.</li>
-    </ul>
-    </li>
-    <li>Adapter:
-    <ul>
-    <li><i>Default Queue</i> - This will use only Dotb\'s Database without any message queue.</li>
-    <li><i>Amazon SQS</i> - Amazon Simple Queue Service is a distributed queue
-    messaging service introduced by Amazon.com.
-    It supports programmatic sending of messages via web service applications as a way to
-    communicate over the Internet.</li>
-    <li><i>RabbitMQ</i> - is open source message broker software (sometimes called message-oriented middleware)
-    that implements the Advanced Message Queuing Protocol (AMQP).</li>
-    <li><i>Gearman</i> - is an open source application framework designed to distribute appropriate computer
-    tasks to multiple computers, so large tasks can be done more quickly.</li>
-    <li><i>Immediate</i> - Like the default queue but executes task immediately after adding.</li>
-    </ul>
-    </li>
-</ul>',
+  'LBL_CONFIG_SERVERS_TEXT' => '<p><b>ផ្នែកកំណត់រចនាសម្ព័ន្ធចម្បង។</b></p>
+  <ul>
+       <li>អ្នករត់៖
+       <ul>
+       <li><i>ស្តង់ដារ</i> - ប្រើដំណើរការតែមួយសម្រាប់កម្មករ។</li>
+       <li><i>ប៉ារ៉ាឡែល</i> - ប្រើដំណើរការមួយចំនួនសម្រាប់កម្មករ។</li>
+       </ul>
+       </li>
+       <li>អាដាប់ទ័រ៖
+       <ul>
+       <li><i>ជួរ​លំនាំដើម</i> - វា​នឹង​ប្រើ​តែ​មូលដ្ឋាន​ទិន្នន័យ​របស់ Dotb ដោយ​គ្មាន​ជួរ​សារ។</li>
+       <li><i>Amazon SQS</i> - Amazon Simple Queue Service គឺជាជួរចែកចាយ
+       សេវាកម្មផ្ញើសារណែនាំដោយ Amazon.com ។
+       វាគាំទ្រការផ្ញើសារតាមកម្មវិធីតាមរយៈកម្មវិធីសេវាកម្មគេហទំព័រជាមធ្យោបាយមួយដើម្បី
+       ទំនាក់ទំនងតាមអ៊ីនធឺណិត។</li>
+       <li><i>RabbitMQ</i> - គឺ​ជា​កម្មវិធី​ឈ្មួញកណ្តាល​សារ​ប្រភព​បើកចំហ (ជួនកាល​គេ​ហៅ​ថា​កម្មវិធី​កណ្តាល​តម្រង់​ទិស​សារ)
+       ដែលអនុវត្តពិធីការតម្រង់ជួរសារកម្រិតខ្ពស់ (AMQP)។</li>
+       <li><i>Gearman</i> - គឺជាកម្មវិធីប្រភពបើកចំហរដែលត្រូវបានរចនាឡើងដើម្បីចែកចាយកុំព្យូទ័រដែលសមស្រប
+       កិច្ចការ​ទៅ​កាន់​កុំព្យូទ័រ​ច្រើន ដូច្នេះ​កិច្ចការ​ធំៗ​អាច​ត្រូវ​បាន​ធ្វើ​កាន់​តែ​លឿន។</li>
+       <li><i>ភ្លាមៗ</i> - ចូលចិត្តជួរលំនាំដើម ប៉ុន្តែដំណើរការភារកិច្ចភ្លាមៗបន្ទាប់ពីបន្ថែម។</li>
+       </ul>
+       </li>
+  </ul>',
   'LBL_CONFIG_SERVERS_TITLE' => 'Job Queue Configuration Help',
   'LBL_CONFIG_TITLE_MODULE_SETTINGS' => 'Job Queue Settings',
   'LBL_DATA' => 'Job Data',
