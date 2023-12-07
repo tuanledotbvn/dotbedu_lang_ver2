96a402eea43ae6bfa912508502e7400c30b0f0c3
ChannboreyMoul00 <152970372+ChannboreyMoul00@users.noreply.github.com>
2023-12-07 13:32:02 +0700

    Update kh_CA.lang.php

--- modules/Connectors/connectors/sources/lms/starmathsonline/language/kh_CA.lang.php
+++ modules/Connectors/connectors/sources/lms/starmathsonline/language/kh_CA.lang.php
@@ -2,7 +2,7 @@
 
 
 $connector_strings = array(
-    'server_host' => 'ម៉ាស៊ីនមេម៉ាស៊ីនមេ',
-    'secure_id' => 'ដេលក្ផានបារម្ក ID',
-    'secure_key' => 'សោសុវត្ថិភាព',
+    'server_host' => 'Server Host',
+    'secure_id' => 'Secure ID',
+    'secure_key' => 'Secure Key',
 );
