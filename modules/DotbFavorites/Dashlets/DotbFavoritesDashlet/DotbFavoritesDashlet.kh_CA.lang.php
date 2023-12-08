c127d79e30da7e6dfb4cb7e503038423b2abe3b1
ChannboreyMoul00 <152970372+ChannboreyMoul00@users.noreply.github.com>
2023-12-08 11:17:14 +0700

    Update DotbFavoritesDashlet.kh_CA.lang.php

--- modules/DotbFavorites/Dashlets/DotbFavoritesDashlet/DotbFavoritesDashlet.kh_CA.lang.php
+++ modules/DotbFavorites/Dashlets/DotbFavoritesDashlet/DotbFavoritesDashlet.kh_CA.lang.php
@@ -6,6 +6,6 @@
 $dashletStrings['DotbFavoritesDashlet'] = array(
                                             'LBL_TITLE'               => 'កំណត់ត្រាដែលខ្ញុំចូលចិត្តបំផុត',
                                             'LBL_HOMEPAGE_TITLE'      => 'Dotb_Favorites',
-                                            'LBL_CONFIGURE_TITLE'  => 'ចមនងចើង',
+                                            'LBL_CONFIGURE_TITLE'  => 'ចំណងជើង',
                                          );
 ?>
