c1b19d1367327902023478da13d0a0aabcf47388
Ngoc Thieu <54256540+ngocthieuff@users.noreply.github.com>
2023-12-11 13:08:33 +0700

    Translate module E part 02 (#6)

--- modules/ExpressionEngine/language/kh_CA.lang.php
+++ modules/ExpressionEngine/language/kh_CA.lang.php
@@ -1,11 +1,11 @@
 <?php
 
 $mod_strings=array (
-  'LBL_EXPAND' => 'Expand',
+  'LBL_EXPAND' => 'ពង្រីក',
   'LBL_MODULE_NAME' => 'Expression Engine',
   'LBL_MODULE_NAME_SINGULAR' => 'Expression Engine',
-  'LBL_NEXT' => 'Next',
-  'LBL_PREVIOUS' => 'Previous',
+  'LBL_NEXT' => 'បន្ទាប់',
+  'LBL_PREVIOUS' => 'ពីមុន',
   'func_descriptions' => 
   array (
   ),
