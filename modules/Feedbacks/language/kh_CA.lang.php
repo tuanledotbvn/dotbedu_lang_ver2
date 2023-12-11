45a41622526e4e72167c7deaf2d94295cbb8f49f
Ngoc Thieu <54256540+ngocthieuff@users.noreply.github.com>
2023-12-11 13:07:01 +0700

    Translate Feedbacks, Filters fte_UsageTracking, History, Holidays, Home, Import, InboundEmail (#7)

--- modules/Feedbacks/language/kh_CA.lang.php
+++ modules/Feedbacks/language/kh_CA.lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $mod_strings=array (
-  'LBL_FEEDBACK_ASIDE_CLICK_MORE' => 'Click here to learn more',
-  'LBL_FEEDBACK_ASIDE_CONTACT_SUPPORT' => 'contact Support',
-  'LBL_FEEDBACK_BTN_SUBMIT' => 'Send',
-  'LBL_FEEDBACK_CSAT' => 'How would you describe your overall satisfaction with this product?',
-  'LBL_FEEDBACK_NOT_SENT' => 'Unable to contact feedback server, please try again later.',
-  'LBL_FEEDBACK_SEND_ERROR' => 'Feedback not sent, please fill in both fields.',
-  'LBL_FEEDBACK_SENT' => 'Feedback sent.',
-  'LBL_FEEDBACK_TEXT_PLACEHOLDER' => 'Feedback description',
-  'TPL_FEEDBACK_ASIDE' => 'We collect certain usage data at the time of submission. {{learnMoreLink}}. We do not collect contact information. Please {{contactSupportLink}} for bugs or other issues that require a response.',
+  'LBL_FEEDBACK_ASIDE_CLICK_MORE' => 'ចុចទីនេះដើម្បីស្វែងយល់បន្ថែម',
+  'LBL_FEEDBACK_ASIDE_CONTACT_SUPPORT' => 'ទាក់ទងផ្នែកជំនួយ',
+  'LBL_FEEDBACK_BTN_SUBMIT' => 'ផ្ញើ',
+  'LBL_FEEDBACK_CSAT' => 'តើអ្នកពណ៌នាការពេញចិត្តជារួមរបស់អ្នកចំពោះផលិតផលនេះដោយរបៀបណា?',
+  'LBL_FEEDBACK_NOT_SENT' => 'មិនអាចទាក់ទងម៉ាស៊ីនមេមតិកែលម្អបានទេ សូមព្យាយាមម្តងទៀតនៅពេលក្រោយ។',
+  'LBL_FEEDBACK_SEND_ERROR' => 'មតិត្រឡប់មិនត្រូវបានផ្ញើទេ សូមបំពេញក្នុងវាលទាំងពីរ។',
+  'LBL_FEEDBACK_SENT' => 'បានផ្ញើមតិត្រឡប់។',
+  'LBL_FEEDBACK_TEXT_PLACEHOLDER' => 'ការពិពណ៌នាមតិកែលម្អ',
+  'TPL_FEEDBACK_ASIDE' => 'យើងប្រមូលទិន្នន័យប្រើប្រាស់ជាក់លាក់នៅពេលដាក់ស្នើ។ {{learnMoreLink}}។ យើងមិនប្រមូលព័ត៌មានទំនាក់ទំនងទេ។ សូម {{contactSupportLink}} សម្រាប់កំហុស ឬបញ្ហាផ្សេងទៀតដែលត្រូវការការឆ្លើយតប។',
 );
